9fc4ae191e1fbd566a4f01347457d7c69115897f
Anaycoder <84620347+Anaycoder@users.noreply.github.com>
2022-05-19 15:01:09 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCEwMuCqtD65nUTGQoqCBQ2emRL9-VGUdU",
+    authDomain: "pro93lets-chat-kwitter.firebaseapp.com",
+    databaseURL: "https://pro93lets-chat-kwitter-default-rtdb.firebaseio.com",
+    projectId: "pro93lets-chat-kwitter",
+    storageBucket: "pro93lets-chat-kwitter.appspot.com",
+    messagingSenderId: "223284555274",
+    appId: "1:223284555274:web:eb89a0968b2730ab85f9f3"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
